2b400aba2d41a07f93a312661827d643e174ef1d
Kirill <kirilkanote@gmail.com>
2025-12-02 22:11:51 +0300

    create connection with front :)

--- src/db/querytable.py
+++ src/db/querytable.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import String, DATETIME
 
 from fastapi import APIRouter
 from sqlalchemy import text, create_engine, Column, Integer, Float, Table
@@ -29,6 +29,37 @@ class Pfhd(authorizeModule.Base):
     sumsecondyearplper = Column(Float)
     finyear = Column(String)
 
+class fkform(authorizeModule.Base):
+    #engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
+
+    #__table__ = Table('pfhd_spravki', Base.metadata, autoload_with=engine, schema='diplom')
+    __table_args__ = {'schema': 'cbias_spravki'}
+    __tablename__ = 'fk_form'
+
+    total = Column(Float)
+    blocksubbo_inn = Column(String, primary_key=True)
+    strcode = Column(String)
+    codeanalytic = Column(String)
+    period = Column(String)
+    periodicity = Column(String)
+
+class protocols(authorizeModule.Base):
+    #engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
+
+    #__table__ = Table('pfhd_spravki', Base.metadata, autoload_with=engine, schema='diplom')
+    __table_args__ = {'schema': 'cbias_spravki'}
+    __tablename__ = 'protocols'
+
+    protocols_id = Column(String)
+    report_date = Column(DATETIME)
+    year_finance = Column(String)
+    kbk = Column(String)
+    subsidyictype = Column(String)
+    inn = Column(String, primary_key=True)
+    finance = Column(Float)
+    taxes = Column(Float)
+    finance_without_tax = Column(Float)
+
 
 def get_session():
     engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
@@ -46,14 +77,75 @@ def get_session():
 @router.get("/getpfhd",
             description="Получение данных по ПФХД",
             summary="Получение данных по ПФХД")
-def get_pfhd(limit_str):
-
+def get_pfhd():
+    limit_str=10000
     session = get_session()
     pfhds = session.query(Pfhd).limit(limit_str).all()
+    #pfhds = session.query(Pfhd).all()
 
     result = [u.__dict__ for u in pfhds]
     for r in result:
         r.pop('_sa_instance_state', None)
 
     return result
+
+@router.get("/getfkform",
+            description="Получение данных по 737 форме",
+            summary="Получение данных по 737 форме")
+def get_fkform():
+    limit_str=10000
+    session = get_session()
+    pfhds = session.query(fkform).limit(limit_str).all()
+    #pfhds = session.query(Pfhd).all()
+
+    result = [u.__dict__ for u in pfhds]
+    for r in result:
+        r.pop('_sa_instance_state', None)
+
+    return result
+
+@router.get("/getprotocols",
+            description="Получение данных по Протоколам БК",
+            summary="Получение данных по Протоколам БК")
+def get_protocols():
+    limit_str=10000
+    session = get_session()
+    pfhds = session.query(protocols).limit(limit_str).all()
+    #pfhds = session.query(Pfhd).all()
+
+    result = [u.__dict__ for u in pfhds]
+    for r in result:
+        r.pop('_sa_instance_state', None)
+        r.pop('protocols_id', None)
+
+    return result
+
+@router.get("/api/pfhd-chart")
+def get_pfhd_chart():
+    session = get_session()
+    limit_str = 50000
+    session = get_session()
+    rows = session.query(Pfhd).limit(limit_str).all()
+
+    #rows = session.query(Pfhd).all()
+
+    # Сортируем по году
+    rows_sorted = sorted(rows, key=lambda x: x.finyear)
+
+    years = [r.finyear for r in rows_sorted]
+    values = [r.sumcurfinyear for r in rows_sorted]
+
+    return {
+        "title": "ПФХД Данные по годам",
+        "xtitle": "Годы",
+        "ytitle": "Сумма текущий год",
+        "categories": years,
+        "series": [
+            {
+                "name": "sumcurfinyear",
+                "data": values
+            }
+        ]
+    }
+
 
